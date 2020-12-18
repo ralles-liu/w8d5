51c5718bb05f45933c9d336a2567fb2bc825feaa
Zihao Li <pockyhao518@gmail.com>
2020-12-18 15:45:17 -0500

    BUBBLE SORT

--- callback_exercises.js
+++ callback_exercises.js
@@ -63,3 +63,51 @@ function addNumbers(sum, numsLeft, completionCallback) {
 }
 
 // addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+
+function askIfGreaterThan(el1, el2, callback){
+    reader.question(`Is ${el1} greater than ${el2}? Answer: `, function(string){
+        if (string === 'yes'){
+            callback(true);
+        }else{
+            callback(false);
+        }
+    })
+    
+}
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop){
+    if (i < arr.length -1){
+        askIfGreaterThan(arr[i], arr[i + 1], isGreaterThan => {
+            if (isGreaterThan){
+                let temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                innerBubbleSortLoop(arr, i + 1, true, outerBubbleSortLoop);
+            }else{
+                innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
+            }
+        })
+    }else if (i === arr.length - 1){
+        outerBubbleSortLoop(madeAnySwaps);
+    }
+}
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+    function  outerBubbleSortLoop(madeAnySwaps){
+      // Begin an inner loop if you made any swaps. Otherwise, call
+      // `sortCompletionCallback`.
+      if(madeAnySwaps){
+          innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+      }else{
+          sortCompletionCallback(arr);
+      }
+    }
+  
+    // Kick the first outer loop off, starting `madeAnySwaps` as true.
+    outerBubbleSortLoop(true)
+  }
+  
+  absurdBubbleSort([3, 2, 1], function(arr) {
+    console.log("Sorted array: " + JSON.stringify(arr));
+    reader.close();
+  });
