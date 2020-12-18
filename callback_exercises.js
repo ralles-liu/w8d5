cefc405184e2cd53e471d52a46eb33cfad748f57
Zihao Li <pockyhao518@gmail.com>
2020-12-18 17:49:56 -0500

    finished

--- callback_exercises.js
+++ callback_exercises.js
@@ -107,7 +107,90 @@ function absurdBubbleSort(arr, sortCompletionCallback) {
     outerBubbleSortLoop(true)
   }
   
-  absurdBubbleSort([3, 2, 1], function(arr) {
-    console.log("Sorted array: " + JSON.stringify(arr));
-    reader.close();
-  });
+//   absurdBubbleSort([3, 2, 1], function(arr) {
+//     console.log("Sorted array: " + JSON.stringify(arr));
+//     reader.close();
+//   });
+
+Function.prototype.myBind = function(context){
+    return () => this.apply(context)
+}
+
+
+class Lamp {
+    constructor() {
+      this.name = "a lamp";
+    }
+  }
+  
+  const turnOn = function() {
+    console.log("Turning on " + this.name);
+  };
+  
+  const lamp = new Lamp();
+  
+//   turnOn(); // should not work the way we want it to
+  
+  const boundTurnOn = turnOn.bind(lamp);
+  const myBoundTurnOn = turnOn.myBind(lamp);
+  
+//   boundTurnOn(); // should say "Turning on a lamp"
+//   myBoundTurnOn();
+
+
+
+Function.prototype.myThrottle = function(interval) {
+  let tooSoon = false;
+  return (...args) => {
+    if (tooSoon === false){
+        tooSoon = true;
+        setTimeout(() => tooSoon = false, interval);
+        this(...args);
+        }
+    }
+}
+
+class Neuron {
+    fire() {
+      console.log("Firing!");
+    }
+  }
+  
+  const neuron = new Neuron();
+  // When we create a new Neuron,
+  // we can call #fire as frequently as we want
+  
+  // The following code will try to #fire the neuron every 10ms. Try it in the console:
+  const interval = setInterval(() => {
+    neuron.fire();
+  }, 10);
+  
+  // You can use clearInterval to stop the firing:
+//   clearInterval(interval);
+  
+  // Using Function#myThrottle, we should be able to throttle
+  // the #fire function of our neuron so that it can only fire
+  // once every 500ms:
+  
+  neuron.fire = neuron.fire.myThrottle(500);
+  
+  Function.prototype.myDebounce = function(interval) {
+    // declare a variable outside of the returned function
+    let timeout;
+    // return a function that takes an arbitrary number of arguments
+    return (...args) => {
+      // declare a function that sets timeout to null and invokes this with args
+      const fnCall = () => {
+        timeout = null;
+        this(...args);
+      }
+      // each time this function is called, it will clear the previous timeout
+      // and create a new one that invokes fnCall after the interval has passed
+      // since the timeout is reset every time the function is invoked, 
+      // fnCall will only be called once the interval has passed without any new 
+      // invocations
+      clearTimeout(timeout);
+      timeout = setTimeout(fnCall, interval);
+    }
+  }
+  
